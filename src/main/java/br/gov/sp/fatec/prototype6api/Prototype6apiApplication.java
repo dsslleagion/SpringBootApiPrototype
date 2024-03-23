324fbf6922a78ac32345021ba04e673c25746f67
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-03-23 19:23:49 -0300

    up

---
+++ src/main/java/br/gov/sp/fatec/prototype6api/Prototype6apiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.prototype6api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prototype6apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prototype6apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/prototype6api/Prototype6apiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.prototype6api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prototype6apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
